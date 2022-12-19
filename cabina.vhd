69e195aed520d9ac86f05d28a5cb73347d8479c4
Pedro Luis Castedo Cepeda <pedroluis.castedo@upm.es>
2022-12-19 17:56:50 +0100

    Refactor CABINA

--- cabina.vhd
+++ cabina.vhd
@@ -1,104 +1,83 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.numeric_std.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-entity Cabina is
-    Port (
-        clk                : in  std_logic;
-        reset              : in  std_logic;
-        Aviso_ascensor     : in  std_logic_vector(1 downto 0);--00 parado, 01 bajando, 10 subiendo, 11 nada
-        Aviso_motor_puerta : in  std_logic;--0 cerrar,1 abrir
-        --Piso_objetivo: out std_logic_vector(1 downto 0);--Memoria boton
-        Piso_actual        : out std_logic_vector(1 downto 0);
-        Feedback_puerta    : out std_logic;-- para pobrear es una señal
-        --Feedback_motor: out std_logic_vector(2 downto 0);
-        --cont: out std_logic_vector(1 downto 0)
-        Salida_leds_motor  : out std_logic_vector(1 downto 0);
-        Salida_leds_puertas: out std_logic;
-        Salida_barrido     : out std_logic_vector(5 downto 0);
-        Luz_emer_motor     : out std_logic;
-        Luz_emer_puertas   : out std_logic
+entity CABINA is
+    port (
+        -- Coomon I/O
+        RESET_N          : in  std_logic;
+        CLK              : in  std_logic;
+
+        -- Cabin motor I/O
+        CE_CABIN         : in  std_logic;
+        UP               : in  std_logic;
+        DN               : in  std_logic;
+        FLOOR            : out std_logic_vector(3 downto 0);
+        LUZ_EMER_MOTOR   : out std_logic;
+
+        -- Door motor I/O
+        CE_DOOR          : in  std_logic;
+        DO_OPEN          : in  std_logic;
+        DO_CLOSE         : in  std_logic;
+        FULL_OPEN        : out std_logic;
+        FULL_CLOSED      : out std_logic;
+        DOOR_POS         : out std_logic_vector(7 downto 0);
+        LUZ_EMER_PUERTAS : out std_logic
     );
-end Cabina;
+end CABINA;
 
-architecture Structural of Cabina is
-
-    signal S_Aviso_puertas: std_logic;
-    -- signal S_Aviso_Ascensor: std_logic_vector(1 downto 0);
+architecture STRUCTURAL of CABINA is
    
-    component motor 
-        port(
-            Aviso_ascensor      : in  std_logic_vector(1 downto 0);--00 parado, 01 bajando, 10 subiendo, 11 nada
-            clk                 : in  STD_LOGIC;
-            reset               : in  STD_LOGIC;
-            Movimiento_ascensor : out std_logic_vector (1 downto 0);-- 00 parado, 10 para encender led subir y 01 para encender led de bajar, 11 nad
-            Luz_emergencia_m    : out std_logic
-        );
-    end component;
-
-    component motor_puertas
-        port
-         (
-             Aviso_puertas      : in  std_logic;--0 cerrar,1 abrir
-             clk                : in  STD_LOGIC;
-             reset              : in  STD_LOGIC;
-             Movimiento_puertas : out std_logic;--0 cerrando, 1 abriendo
-             Luz_emergencia_p   : out std_logic
-         );
-    end component;
-
-    component Comparador 
+    component MOTOR_CABINA is
         port (
-            clk                : in  std_logic;
-            reset              : in  std_logic;
-            Aviso_ascensor     : in  std_logic_vector(1 downto 0);--00 parado, 01 bajando, 10 subiendo, 11 nada
-            Aviso_motor_puerta : in  std_logic;--0 cerrar,1 abrir
-            --Piso_objetivo: out std_logic_vector(1 downto 0);--Memoria boton
-            Piso_actual        : out std_logic_vector(1 downto 0)
-        );
+            RESET_N          : in  std_logic;
+            CLK              : in  std_logic;
+            CE               : in  std_logic;
+            UP               : in  std_logic;
+            DN               : in  std_logic;
+            FLOOR            : out std_logic_vector(3 downto 0);
+            LUZ_EMERGENCIA_M : out std_logic
+        );   
     end component;
 
-    component BarridoLed
-        port(
-            clk 		  : in  std_logic;
-            --reset 		: in  std_logic;
-            Aviso_puerta  : in  std_logic; --1 abrir 0 cerrar
-            Estado_puerta : out std_logic;
-            LED 		  : out std_logic_vector(5 downto 0) --ceros abierta, unos cerrada
+    component MOTOR_PUERTAS is
+        port ( 
+            RESET_N          : in  std_logic;
+            CLK              : in  std_logic;
+            CE               : in  std_logic;
+            DO_OPEN          : in  std_logic;
+            DO_CLOSE         : in  std_logic;
+            FULL_OPEN        : out std_logic;
+            FULL_CLOSED      : out std_logic;
+            DOOR_POS         : out std_logic_vector(7 downto 0);
+            LUZ_EMERGENCIA_P : out std_logic
         );
-    end component ;   
+    end component;
 
 begin
 
-    inst_motor: motor port map(
-         Aviso_ascensor => Aviso_Ascensor,
-         clk => clk, 
-         reset => reset,
-         Movimiento_ascensor => Salida_leds_motor,
-         Luz_emergencia_m => Luz_emer_motor     
-         );
+    cabin_motor0: MOTOR_CABINA
+        port map (
+            RESET_N          => RESET_N,
+            CLK              => CLK,
+            CE               => CE_CABIN,
+            UP               => UP,
+            DN               => DN,
+            FLOOR            => FLOOR,
+            LUZ_EMERGENCIA_M => LUZ_EMER_MOTOR
+        );
 
-    inst_motor_puertas: motor_puertas port map(
-       Aviso_puertas => Aviso_motor_puerta, 
-       clk => clk,
-       reset => reset,
-       Movimiento_puertas => S_Aviso_puertas,
-       Luz_emergencia_p => Luz_emer_puertas
-    );
-     
-    inst_Comparador: Comparador port map( 
-        clk => clk,
-        reset => reset,
-        Aviso_ascensor => Aviso_ascensor,--00 parado, 01 bajando, 10 subiendo, 11 nada
-        Aviso_motor_puerta => Aviso_motor_puerta,--0 cerrar,1 abrir
-        --Piso_objetivo: out std_logic_vector(1 downto 0);--Memoria boton
-        Piso_actual => Piso_actual
-    );
-        
-    inst_BarridoLed: BarridoLed port map(
-        clk =>clk,
-      	Aviso_puerta => S_Aviso_puertas,--1 abrir 0 cerrar
-        Estado_puerta => Feedback_puerta,
-        LED => Salida_barrido --ceros abierta, unos cerrada
-    );
+    door_motor0: MOTOR_PUERTAS
+        port map ( 
+            RESET_N          => RESET_N,
+            CLK              => CLK,
+            CE               => CE_DOOR,
+            DO_OPEN          => DO_OPEN,
+            DO_CLOSE         => DO_CLOSE,
+            FULL_OPEN        => FULL_OPEN,
+            FULL_CLOSED      => FULL_CLOSED,
+            DOOR_POS         => DOOR_POS,
+            LUZ_EMERGENCIA_P => LUZ_EMER_PUERTAS
+        );
+
 end architecture Structural;
