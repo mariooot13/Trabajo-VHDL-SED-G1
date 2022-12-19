cfb1074f4ab0cb66543993c4b5947cb7619076ea
Pedro Luis Castedo Cepeda <pedroluis.castedo@upm.es>
2022-12-19 17:56:17 +0100

    Refactor MOTOR_PUERTAS

--- motor_puertas.vhd
+++ motor_puertas.vhd
@@ -1,31 +1,43 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
+entity MOTOR_PUERTAS is
+    port ( 
+        RESET_N          : in  std_logic;
+        CLK              : in  std_logic;
+        CE               : in  std_logic;
+        DO_OPEN          : in  std_logic;
+        DO_CLOSE         : in  std_logic;
+        FULL_OPEN        : out std_logic;
+        FULL_CLOSED      : out std_logic;
+        DOOR_POS         : out std_logic_vector(7 downto 0);
+        LUZ_EMERGENCIA_P : out std_logic
+    );
+end MOTOR_PUERTAS;
 
-entity motor_puertas is
-    Port ( 
-           Aviso_puertas : in std_logic;--0 cerrar,1 abrir
-           clk : in STD_LOGIC;
-           reset : in STD_LOGIC;
-           Movimiento_puertas: out std_logic;--0 cerrando, 1 abriendo
-           Luz_emergencia_p: out std_logic
-           );
-
-end motor_puertas;
-
-architecture Behavioral of motor_puertas is
-
+architecture BEHAVIORAL of MOTOR_PUERTAS is
+  type door_frame_vector is array (natural range <>) of std_logic_vector(DOOR_POS'range);
+  constant door_frames : door_frame_vector := ("00000000", "10000001", "11000011", "11100111", "11111111");   
+  subtype pos_t is door_frames'range;
+  signal pos : pos_t;
 begin
-process(clk,reset)
-  begin
-  if (reset='1') then
-     Luz_emergencia_p<='1'; 
-     Movimiento_puertas<='0';
-  elsif (rising_edge(clk)) then
-          Movimiento_puertas<=Aviso_puertas;
+    process(CLK, RESET_N)
+    begin
+        if RESET_N = '0' then
+            pos <= 0; -- Door initial state: open
+            LUZ_EMERGENCIA_P <= '0';
+        elsif rising_edge(CLK) then
+            if CE = '1' then
+                if DO_OPEN = '1' and pos > pos_t'low then
+                    pos <= pos - 1;
+                elsif DO_CLOSE = '1' and pos < pos_t'high then
+                    pos <= pos + 1;
+                end if;
+            end if;
+        end if;
+    end process;
 
-   end if;
-
-  end process;
-
-end Behavioral;
+    FULL_OPEN   <= '1' when pos = pos_t'low else '0';
+    FULL_CLOSED <= '1' when pos = pos_t'high else '0';
+    DOOR_POS    <= door_frames(pos);
+end BEHAVIORAL;
