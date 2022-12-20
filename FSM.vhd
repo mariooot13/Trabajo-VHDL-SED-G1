3ff706dcb6df046a4847e18ffae98aa7bb4086b4
Pedro Luis Castedo Cepeda <pedroluis.castedo@upm.es>
2022-12-20 10:54:26 +0100

    Adapt FSM to new I/O configuration

--- FSM.vhd
+++ FSM.vhd
@@ -1,170 +1,119 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
-
-
 entity FSM is
-    Port ( Clk : in STD_LOGIC;
-           Start: in std_logic; --como un enable
-           Reset : in STD_LOGIC;
-           Piso_Actual: in std_logic_vector(1 downto 0);
-           Feedback_puerta : in std_logic; --1 abierto, 0 cerrado
-           Botonera: in std_logic_vector(1 downto 0); -- viene del antirrebote convertida
-           Aviso_motor_movimiento: out std_logic_vector(1 downto 0); --00 parado, 01 bajando, 10 subiendo, 11 nada
-           Aviso_motor_puerta:out std_logic; --1 que se abran,0 que se cierren
-           --Piso_objetivo: out std_logic_vector -- va a ir a un decodificador o lo que sea para que nos imprima como camvbia de piso, se le va asignar luego memoria boton.
-           --semueveono : out std_logic;
-           Aviso_Inicio:out std_logic ;
-           --Piso_objetivo: out std_logic_vector -- va a ir a un decodificador o lo que sea para que nos imprima como camvbia de piso, se le va asignar luego memoria boton.
-           --semueveono : out std_logic;
-           Aviso_Parado: out std_logic;
-           Aviso_Cerrar_puertas: out std_logic;
-           Aviso_Movimiento: out std_logic;
-           Aviso_Abrir:out std_logic
-           
-           );
+    port (
+        RESET_N     : in  std_logic;
+        CLK         : in  std_logic;
+        BUTTONS     : in  std_logic_vector(3 downto 0);
+        FLOOR       : in  std_logic_vector(3 downto 0);
+        FULL_OPEN   : in  std_logic;
+        FULL_CLOSED : in  std_logic;
+        UP          : out std_logic;
+        DN          : out std_logic;
+        DO_OPEN     : out std_logic;
+        DO_CLOSE    : out std_logic
+    );
 end FSM;
 
-architecture Behavioral of FSM is
-    type estado IS (inicial, parado,cerrar_puertas,movimiento,abrir_puertas);
-    signal state : estado := inicial;
-    signal next_state: estado;
-    signal memoria_boton: std_logic_vector(1 downto 0);
-begin
-state_reg: process (Clk,Reset)
-  begin
-    if Reset <='0' then
-        state<=inicial;
-     elsif rising_edge (clk) then 
-        state<=next_state ;
-        end if;
-     end process;
-  
-nxt_state_decoder: process (state,start,Feedback_puerta, Piso_Actual, Botonera)
-  begin
-  next_state <=state;
-    case state is
-     when inicial =>
-        if start='1' then
-          next_state<=parado ;
-          end if;
-      when parado =>
-        if Piso_Actual /= memoria_boton and Feedback_puerta ='1'  then
-          next_state<=cerrar_puertas  ;
-          end if;
-      when cerrar_puertas  =>
-       
-        if Feedback_puerta='0' and Piso_Actual /= memoria_boton then
-          next_state<=movimiento ;
-          end if;    
-      when movimiento   =>
-     
-        if Piso_Actual= memoria_boton  then
-          next_state<=abrir_puertas ;
-          end if;   
-       when abrir_puertas    =>
-      
-        if Feedback_puerta='1'and Piso_Actual = memoria_boton  then
-          next_state<=parado ;
-          end if; 
-      when others =>
-      next_state <= inicial;
-    end case;
-  end process;
-  
-salidas: process(state)
-begin
-    case next_state  is
-    when inicial=>
-    Aviso_inicio<='1';
-     Aviso_Parado<='0';
-     Aviso_Cerrar_puertas<='0';
-     Aviso_Movimiento<='0';
-     Aviso_Abrir<='0';
-     
-     when parado => 
-     --led que se encienden por lo abrir_puertas
-     
-     Aviso_inicio<='0';
-     Aviso_Parado<='1';
-        Aviso_Cerrar_puertas<='0';
-           Aviso_Movimiento<='0';
-           Aviso_Abrir<='0';
-           
-     ----------------
-        Aviso_motor_puerta<='1';
-        Aviso_motor_movimiento<="00";
-     when cerrar_puertas=>
-     -----------
-     Aviso_inicio<='0';
-     Aviso_Parado<='0';
-     Aviso_Cerrar_puertas<='1';
-     Aviso_Movimiento<='0';
-      Aviso_Abrir<='0';
-      ----------------
-         Aviso_motor_puerta<='0';
-         Aviso_motor_movimiento<="00";
-      when movimiento=>
-       
-       if(memoria_boton > Piso_Actual) then
-       ---------------
-        Aviso_inicio<='0';
-     Aviso_Parado<='0';
-        Aviso_Cerrar_puertas<='0';
-           Aviso_Movimiento<='1';
-           Aviso_Abrir<='0';
-           --------------
-            Aviso_motor_puerta<='0';
-            Aviso_motor_movimiento<="10";
-            --semueveono <= '1';--que subas
-        else
-        ----------------
-         Aviso_inicio<='0';
-         Aviso_Parado<='0';
-          Aviso_Cerrar_puertas<='0';
-           Aviso_Movimiento<='1';
-           Aviso_Abrir<='0';
-           --------------
-            Aviso_motor_puerta<='0';
-            Aviso_motor_movimiento<="01";
-            --semueveono <= '1';-- que bajes
-        end if;
-        
-       when abrir_puertas =>
-       ---------------
-        Aviso_inicio<='0';
-     Aviso_Parado<='0';
-        Aviso_Cerrar_puertas<='0';
-           Aviso_Movimiento<='0';
-           Aviso_Abrir<='1';
-           ------------
-            Aviso_motor_puerta<='1';
-            Aviso_motor_movimiento<="00";
-      when others =>
-           Aviso_motor_movimiento<="11";
-     end case;
-  end process;
-   
- 
-memoria: process (reset,botonera,clk)-- clk dudoso
-begin
-    if Reset='0' then
-        memoria_boton<="00";
-    elsif rising_edge(Clk) then 
-        if (Botonera <="00") then
-            memoria_boton<="00";
-        elsif(Botonera <="01") then
-            memoria_boton<="01";
-        elsif(Botonera <="10") then
-            memoria_boton<="10";
-         elsif(Botonera <="11") then
-            memoria_boton<="11";
-       end if;
-    end if;
+architecture BEHAVIORAL of FSM is
 
+    type state_t IS (S0_INITIAL, S1_CLOSING, S2_GOUP, S3_GODN, S4_OPENING);
+    signal nxt_state, state       : state_t;
+    signal nxt_btn_memo, btn_memo : std_logic_vector(3 downto 0);
+
+begin
+
+    state_reg: process (CLK, RESET_N)
+    begin
+        if RESET_N = '0' then
+            state    <= S0_INITIAL;
+            btn_memo <= (others => '0');
+        elsif rising_edge(CLK) then
+            state    <= nxt_state;
+            btn_memo <= nxt_btn_memo;
+        end if;
     end process;
-end Behavioral;
+
+    nxt_state_decoder: process (BUTTONS, FLOOR, FULL_OPEN, FULL_CLOSED, btn_memo, state)
+    begin
+        nxt_state    <= state;
+        nxt_btn_memo <= btn_memo;
+        case state is
+            when S0_INITIAL =>
+                if BUTTONS /= "0000" and BUTTONS /= FLOOR then
+                    nxt_state <= S1_CLOSING;
+                    nxt_btn_memo  <= BUTTONS;
+                end if;
+
+            when S1_CLOSING =>
+                if FULL_CLOSED = '1' then
+                    if btn_memo > FLOOR then
+                        nxt_state <= S2_GOUP;
+                    else
+                        nxt_state <= S3_GODN;
+                    end if;
+                end if;
+
+            when S2_GOUP | S3_GODN =>
+                if btn_memo = FLOOR then
+                    nxt_state <= S4_OPENING;
+                end if;
+
+            when S4_OPENING =>
+                if FULL_OPEN = '1'  then
+                    nxt_state <= S0_INITIAL;
+                end if;
+
+            when others =>
+                nxt_state <= S0_INITIAL;
+        end case;
+    end process;
+  
+    salidas: process(state)
+    begin
+        UP       <= '0';
+        DN       <= '0';
+        DO_OPEN  <= '0';
+        DO_CLOSE <= '0';
+        case state is
+            when S0_INITIAL =>
+                UP       <= '0';
+                DN       <= '0';
+                DO_OPEN  <= '0';
+                DO_CLOSE <= '0';
+    
+            when S1_CLOSING => 
+                UP       <= '0';
+                DN       <= '0';
+                DO_OPEN  <= '0';
+                DO_CLOSE <= '1';
+    
+            when S2_GOUP =>
+                UP       <= '1';
+                DN       <= '0';
+                DO_OPEN  <= '0';
+                DO_CLOSE <= '0';
+    
+            when S3_GODN =>
+                UP       <= '0';
+                DN       <= '1';
+                DO_OPEN  <= '0';
+                DO_CLOSE <= '0';
+    
+            when S4_OPENING =>
+                UP       <= '0';
+                DN       <= '0';
+                DO_OPEN  <= '1';
+                DO_CLOSE <= '0';
+    
+            when others =>
+                UP       <= '0';
+                DN       <= '0';
+                DO_OPEN  <= '0';
+                DO_CLOSE <= '0';
+        end case;
+    end process;
+
+end BEHAVIORAL;
