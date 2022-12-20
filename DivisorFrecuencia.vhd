be61ed883c3449897a440f004d7710e4ad651707
Pedro Luis Castedo Cepeda <pedroluis.castedo@upm.es>
2022-12-20 10:53:38 +0100

    Refactor DivisorFrecuencia

--- DivisorFrecuencia.vhd
+++ DivisorFrecuencia.vhd
@@ -4,37 +4,37 @@ use ieee.numeric_std.all;
 
 entity DivisorFrecuencia is
     generic (
-        FREQ_ENTRADA : integer;
-        FREQ_SALIDA	 : integer
+        FREQ_IN  : integer;
+        FREQ_OUT : integer
 	);
     port (
-        clk_entrada : in  std_logic; 
-        reset_n     : in  std_logic; 
-        clk_salida  : out std_logic
+        CLK_IN  : in  std_logic; 
+        RESET_N : in  std_logic; 
+        CLK_OUT : out std_logic
  );
 end DivisorFrecuencia;
 
 architecture Behavioral of DivisorFrecuencia is
-    signal salida   : std_logic;
+    signal clk_out_i   : std_logic;
     signal contador : integer;
     signal razon    : integer;
     signal cuenta   : integer;
 begin	
-	process (reset, clk_entrada)
-	    constant SEMIPERIOD : positive := FREQ_ENTRADA / (2 * FREQ_SALIDA);
+	process (RESET_N, CLK_IN)
+	    constant SEMIPERIOD : positive := FREQ_IN / (2 * FREQ_OUT);
 	    subtype count_t is integer range 0 to SEMIPERIOD - 1;
 	    variable count : count_t;
 	begin
-        if reset_n = '0' then
+        if RESET_N = '0' then
             count := 0;
-			salida <= '0';
-        elsif rising_edge(clk_entrada) then
+			clk_out_i <= '0';
+        elsif rising_edge(CLK_IN) then
             count := (count + 1) mod SEMIPERIOD;
             if count = 0 then
-                salida <= not salida;
+                clk_out_i <= not clk_out_i;
             end if;
         end if;
 	end process;
      
-    clk_salida <= salida;
+    CLK_OUT <= clk_out_i;
 end Behavioral;
